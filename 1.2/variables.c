07e2e5b4cff1c35d25cff788564cf70b5f7dfd8e
Victor <vicsingh305@gmail.com>
2016-09-30 15:30:13 -0400

    explained concepts inside 1.2 source

--- 1.2/variables.c
+++ 1.2/variables.c
@@ -2,9 +2,9 @@
 
 main(){
 
-  int fahr, celcius;
-  int lower, upper, step;
-  
+  int fahr, celcius;// declared variables fahr and celcius
+  int lower, upper, step;//declared lowst temperature, highest temperature and step
+  //what is step? Step is what we incrament fahr by until the condition in the while loop turns false
   lower = 0;
   upper = 300;
   step = 20;
