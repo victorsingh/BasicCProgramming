c91363688674075c026885d42cdf17869a414ac5
Victor <vicsingh305@gmail.com>
2016-09-30 15:17:22 -0400

    Added 1.1 and 1.2, first commit to repository

---
+++ 1.1/helloworld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>// this tells the compiler to include information about the standard input/output library ; this line appears the beginning or many C source files
+
+main(){// this is a defined function that receives no argument values
+
+  printf("hello world");// this calls the main library function printf to print this sequence of characters \n represents newline character 
+
+}
+
+
---
+++ 1.2/variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+main(){
+
+  int fahr, celcius;
+  int lower, upper, step;
+  
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+
+  fahr = lower; 
+  while( fahr <= upper) {
+      celcius = 5 * (fahr-32)/9;
+      printf("%d\t%d\n", fahr, celcius);
+      fahr = fahr + step;
+  }
+}
